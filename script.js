43d6534415c008cfe8ef55ae046b2604a47dfaad
shivam-2008 <shivamyadav142311@gmail.com>
2025-02-03 22:19:33 +0530

    xhnfvbcn
    hnfgxn

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector('.container');
+const but = document.querySelector('.buta');
+
+function buta() {
+    container.classList.add('container-2');
+    but.classList.remove('buta');
+    but.classList.add('buta-2');
+}
+
